f3452576623398e4b9e134e964c7cdf314045bd3
Nischitha Gudigar <nischithagudigardr@gmail.com>
2021-07-24 22:32:48 +0530

    changed product type to product [] type

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -11,8 +11,8 @@ export class ProductService {
 
   constructor(private _http:Http) { }
 
-  getProducts():Observable<Product>{
-    return this._http.get(this._productsUrl).map((response)=><Product>response.json());
+  getProducts():Observable<Product[]>{
+    return this._http.get(this._productsUrl).map((response)=><Product[]>response.json());
   }
   getAlbum(id:number):Observable<Album>{
     return this._http.get(this._albumUrl).map((response)=><Album>response.json());
